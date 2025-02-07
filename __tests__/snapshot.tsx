40615951963804566bff479e15e48e2adfc91cbc
Aroso Emmanuel <emmanueladedejiaroso@gmail.com>
2025-02-07 23:30:22 +0100

    delete unnecessaru files

--- __tests__/snapshot.tsx
+++
@@ -1,9 +0,0 @@
-import { render,screen } from '@testing-library/react'
-import LandingPage from '../src/pages/home'
-import React from 'react'
- 
-
-test("renders homepage", () => {
-  render(<LandingPage />);
-  expect(screen.getByText("Welcome to Next.js 15!")).toBeInTheDocument();
-});
