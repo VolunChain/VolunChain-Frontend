6d9167378ecad0a1669e26888fd4229b18002f2d
alejgonza04 <149022594+alejgonza04@users.noreply.github.com>
2025-02-17 21:41:27 -0500

    Updated baseApi.ts with latest API changes

--- src/api/baseApi.ts
+++ src/api/baseApi.ts
@@ -1,15 +1,18 @@
-import axios, { AxiosInstance } from "axios";
-
+import axios from "axios";
+import type { AxiosInstance, AxiosRequestConfig } from "axios";
+ 
 export class BaseApi {
-  protected axios: AxiosInstance;
+  protected httpClient: AxiosInstance;
 
-  constructor(baseUrl: string = process.env.NEXT_PUBLIC_BASE_URL || "") {
-    this.axios = axios.create({
-      baseURL: baseUrl,
+  constructor(baseUrl?: string) {
+    const config: AxiosRequestConfig = {
+      baseURL: baseUrl ?? process.env.NEXT_PUBLIC_BASE_URL ?? "",
       timeout: 10000,
       headers: {
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
-    });
+    };
+    this.httpClient = axios.create(config)
   }
 }
