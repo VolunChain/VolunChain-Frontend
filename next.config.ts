25da6a13c84cadedb223dc12f8b695d0f22f92ae
Bigcedar <godwinekoh@gmail.com>
2025-02-24 20:48:45 +0100

    Implement right side view Implement Right Side of view store

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'hebbkx1anhila5yf.public.blob.vercel-storage.com',
-      'placehold.co'
-    ],
+    domains: ["hebbkx1anhila5yf.public.blob.vercel-storage.com"],
   },
 };
 
