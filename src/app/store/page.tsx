3b1660b63fa2698e3be2184725adacfc79bf7703
Bigcedar <godwinekoh@gmail.com>
2025-02-27 20:34:39 +0100

    chores: fixed right-side of store

---
+++ src/app/store/page.tsx
@@ -0,0 +1,9 @@
+import StoreView from "@/components/store/view/StoreView";
+
+export default function StorePage() {
+  return (
+    <main className="min-h-screen bg-[#07081f]">
+      <StoreView />
+    </main>
+  );
+}
