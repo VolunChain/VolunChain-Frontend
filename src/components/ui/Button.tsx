26970bfd266b4fb8a263d7ddffa052f3df1c6894
Vlada <hello.vladasana@gmail.com>
2025-03-28 16:03:38 -0300

    aligned buttons and make everything match the design match the desesign

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,8 +5,8 @@ interface ButtonProps {
   onClick?: () => void;
   variant: "primary" | "secondary" | "tertiary" | "outline";
   type?: "button" | "submit" | "reset";
-  textColor?: "white" | "black" | "primary" | "secondary";
-  className?: string 
+  textColor?: "white" | "black" | "primary" | "secondary" | "blue";
+  className?: string;
 }
 
 const Button = ({
@@ -15,27 +15,29 @@ const Button = ({
   variant,
   textColor = "white",
   type = "button",
-  className
+  className,
 }: ButtonProps) => {
   return (
     <button
       type={type}
-      className={clsx("cursor-pointer py-3 px-6 rounded-full w-auto font-semibold border-2", className, {
-        
-        "text-white": textColor === "white",
-        "text-black": textColor === "black",
-        "text-primary": textColor === "primary",
-        "text-secondary": textColor === "secondary",
+      className={clsx(
+        "cursor-pointer py-3 px-6 rounded-full w-auto font-semibold border-2",
+        className,
+        {
+          "text-white": textColor === "white",
+          "text-black": textColor === "black",
+          "text-primary": textColor === "primary",
+          "text-secondary": textColor === "secondary",
+          "text-blue-500": textColor === "blue",
 
-        "bg-primary ":
-          variant === "primary",
-        "border-primary": variant === "primary",
-        "border-2 border-secondary":
-          variant === "secondary",
-        "border-2 border-link bg-link":
-          variant === "tertiary",
-        "w-full flex items-center justify-center gap-6 bg-transparent border border-[#73b9eb] hover:bg-[#73b9eb]/10 transition-colors": variant === "outline",
-      })}
+          "bg-primary ": variant === "primary",
+          "border-primary": variant === "primary",
+          "border-2 border-secondary": variant === "secondary",
+          "border-2 border-link bg-link": variant === "tertiary",
+          "w-full flex items-center justify-center gap-6 bg-transparent border border-[#73b9eb] hover:bg-[#73b9eb]/10 transition-colors":
+            variant === "outline",
+        }
+      )}
       onClick={onClick}
     >
       {children}
