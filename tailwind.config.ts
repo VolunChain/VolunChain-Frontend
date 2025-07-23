f4780dc7b48b67087b53b72adbaf74da0c88eece
villarley <santivillarley1010@gmail.com>
2025-07-23 15:30:22 -0600

    chore: add husky to package.json for Git hooks management

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,8 +3,9 @@ import type { Config } from "tailwindcss";
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/shared/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   darkMode: "class",
   theme: {
