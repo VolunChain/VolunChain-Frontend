52719ff0b928c40a39c6316e6dbb88cc399fb3bf
Juanjom82 <juanjomora1268@outlook.com>
2025-09-30 01:31:42 -0600

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,26 @@
+"use client";
 import Notfound from "@/shared/components/ui/Notfound";
+import Button from "@/shared/components/ui/Button";
 import React from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
-  return <Notfound />;
+  const router = useRouter();
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-[#07081F] dark:text-white relative px-4 text-center">
+      <Notfound />
+
+      <div className="mt-8">
+        <Button
+          variant="primary"
+          onClick={() => router.push("/")}
+        >
+          Go to Homepage
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
